fb8f9c497d7ddcd3c359754314e13dc38d651425
bsametarman@outlook.com <bsametarman@outlook.com>
2021-08-31 13:21:51 +0300

    time feature added

--- MessageSender.py
+++ MessageSender.py
@@ -12,36 +12,37 @@
 """
 
 import webbrowser
-from numpy import NAN
 import pandas as pd
 import pyautogui as pg
 import time
 
-def findPosition():
-    if pg.locateOnScreen('./search.png'):
-        location = pg.center(pg.locateOnScreen('./search.png'))
-        x = location.x
-        y = location.y
-        return x, y
-        
-    elif pg.locateOnScreen('./search2.png'):
-        location = pg.center(pg.locateOnScreen('./search2.png'))
-        x = location.x
-        y = location.y
-        return x, y
-
-def sendMessage(x, y):
-    pg.click(x, y)
-    pg.typewrite(str(number))
-    time.sleep(2)
-    pg.click(x, y + 140)
-    time.sleep(1)
-    pg.typewrite(message)
+def sendMessage():
+    pg.hotkey('ctrl', 'l')
+    pg.typewrite("https://web.whatsapp.com/send?phone=+" + str(number) + "&text=" + message)
     pg.press('enter')
-    time.sleep(1)
-    pg.click(x, y)
-    pg.hotkey('ctrl', 'a')
-    pg.press('backspace')
+    time.sleep(5)
+    pg.click(screenSize[0]/2, screenSize[1]/2)
+    pg.press('enter')
+    time.sleep(2)
+
+def waitTimeCalculate(hour: int, minute: int):
+
+    webbrowser.open("https://web.whatsapp.com")
+
+    currentTime = time.localtime()
+    if currentTime.tm_hour == 0:
+        currentTime.tm_hour = 24
+    currentSec = (currentTime.tm_hour * 3600) + (currentTime.tm_min * 60) + (currentTime.tm_sec)
+    sendedTotalSec = (hour * 3600) + (minute * 60)
+
+    totalSec = sendedTotalSec - currentSec
+    if totalSec <= 0:
+        left_time = 86400 + totalSec
+    
+    print(f"Messages will be sent {totalSec} Secounds later...")
+
+    time.sleep(totalSec)
+
 
 # Your excel file path (Excel file should have 'Numbers' column)
 print("Enter your excel file path...\n")
@@ -56,13 +57,21 @@ numbersRow = pd.read_excel(path, usecols=["Numbers"])
 leng = pd.read_excel(path)
 leng = len(leng.index)
 
+# Gets screen resulotion
+screenSize = pg.size()
+
 print("Write the message that you want to send...\n")
 message = input()
 
-webbrowser.open("https://web.whatsapp.com")
-time.sleep(10)
-positions = findPosition()
+print("Write the time that you want to send message...\n")
+sendedTime = input()
+sendedTime = sendedTime.split()
+sendedTimeHour = int(sendedTime[0])
+if sendedTimeHour == 0:
+    sendedTimeHour = 24
+sendedTimeMin = int(sendedTime[1])
+waitTimeCalculate(sendedTimeHour, sendedTimeMin)
 
 for x in range(leng):
     number = int(numbersRow.values[x])
-    sendMessage(positions[0], positions[1])
+    sendMessage()
